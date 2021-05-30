fbbf3264346bf0d58df3b11194036daa90f4e3df
gelakinetic <gelakinetic@gmail.com>
2021-05-30 00:52:27 -0400

    Exit bash script if build fails

--- burn-dbg.sh
+++ burn-dbg.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Try to kill putty
 taskkill.exe /IM "putty.exe" /F
+
+# exit when any command fails, then build
+set -e
 idf.py -p $ESP32_C3_PORT -b 2000000 flash
+
+# Start putty
 cmd.exe /C "START /B putty.exe -serial $ESP32_C3_PORT_WIN -sercfg 115200"
