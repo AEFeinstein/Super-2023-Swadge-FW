781fbe4f89c31c837d37b22f54fed8f873fb3163
JVeg199X <jveg199x@gmail.com>
2022-05-01 11:43:47 -0700

    Swap mode order

--- main/swadge_esp32.c
+++ main/swadge_esp32.c
@@ -70,8 +70,8 @@ void swadgeModeEspNowSendCb(const uint8_t* mac_addr, esp_now_send_status_t statu
 swadgeMode* swadgeModes[] =
 {
     &modemainMenu,
-    &modeFighter,
     &modePlatformer,
+    &modeFighter,
     &modeDemo
 };
 
