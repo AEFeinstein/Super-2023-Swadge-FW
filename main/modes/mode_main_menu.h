9354b2e9885d114388e8209d9ab330675f8e9c5c
AEFeinstein <gelakinetic@gmail.com>
2022-06-10 00:52:29 -0400

    Add a main menu that selects modes Switch modes by sleeping and waking Stub sleep functions for emu Always init peripherials for emu

--- main/modes/mode_main_menu.h
+++ main/modes/mode_main_menu.h
@@ -1,6 +1,12 @@
 #ifndef _MODE_MAIN_MENU_H_
 #define _MODE_MAIN_MENU_H_
 
+#include "swadgeMode.h"
+
+extern swadgeMode* swadgeModes[];
+uint8_t getNumSwadgeModes(void);
+void switchToSwadgeMode(uint8_t mode);
+
 extern swadgeMode modemainMenu;
 
 #endif
