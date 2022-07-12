5948bb5cd172072f7666d677c0ba6373e5fe37a2
AEFeinstein <gelakinetic@gmail.com>
2022-07-12 02:00:10 -0400

    Added emu gpio stubs

---
+++ emu/src/emu_gpio.c
@@ -0,0 +1,36 @@
+#include <stdint.h>
+#include "esp_err.h"
+#include "driver/gpio.h"
+
+/**
+ * @brief GPIO common configuration
+ *
+ *        Configure GPIO's Mode,pull-up,PullDown,IntrType
+ *
+ * @param  pGPIOConfig Pointer to GPIO configure struct
+ *
+ * @return
+ *     - ESP_OK success
+ *     - ESP_ERR_INVALID_ARG Parameter error
+ *
+ */
+esp_err_t gpio_config(const gpio_config_t *pGPIOConfig __attribute__((unused)))
+{
+    return ESP_OK;
+}
+
+/**
+ * @brief  GPIO set output level
+ *
+ * @param  gpio_num GPIO number. If you want to set the output level of e.g. GPIO16, gpio_num should be GPIO_NUM_16 (16);
+ * @param  level Output level. 0: low ; 1: high
+ *
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG GPIO number error
+ *
+ */
+esp_err_t gpio_set_level(gpio_num_t gpio_num __attribute__((unused)), uint32_t level __attribute__((unused)))
+{
+    return ESP_OK;
+}
