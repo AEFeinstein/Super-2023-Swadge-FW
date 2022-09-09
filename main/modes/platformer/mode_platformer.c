a7d2bdfae4faeab8671a545dd36cde8697d30718
JVeg199X <swadgedev@github.com>
2022-09-08 22:38:05 -0400

    Adjust HUD

--- main/modes/platformer/mode_platformer.c
+++ main/modes/platformer/mode_platformer.c
@@ -277,14 +277,14 @@ void drawPlatformerHud(display_t *d, font_t *font, gameData_t *gameData)
     snprintf(timeStr, sizeof(timeStr) - 1, "T:%03d", gameData->countdown);
 
     if(gameData->frameCount > 10) {
-        drawText(d, font, c500, "1UP", 16, 2);
+        drawText(d, font, c500, "1UP", 24, 2);
     }
     
-    drawText(d, font, c555, livesStr, 48, 2);
+    drawText(d, font, c555, livesStr, 56, 2);
     drawText(d, font, c555, coinStr, 160, 16);
     drawText(d, font, c555, scoreStr, 8, 16);
-    drawText(d, font, c555, levelStr, 160, 2);
-    drawText(d, font, c555, timeStr, 224, 16);
+    drawText(d, font, c555, levelStr, 152, 2);
+    drawText(d, font, c555, timeStr, 220, 16);
 }
 
 void updateTitleScreen(platformer_t *self)
@@ -560,8 +560,8 @@ void updateGameClear(platformer_t *self){
 
 void drawGameClear(display_t *d, font_t *font, gameData_t *gameData){
     drawPlatformerHud(d, font, gameData);
-    drawText(d, font, c555, "Thanks for playing.", 24, 32);
-    drawText(d, font, c555, "Many more battle scenes", 8, 80);
-    drawText(d, font, c555, "will soon be available!", 8, 96);
-    drawText(d, font, c555, "Bonus 100000pts per life!", 8, 144);
+    drawText(d, font, c555, "Thanks for playing.", 24, 48);
+    drawText(d, font, c555, "Many more battle scenes", 8, 96);
+    drawText(d, font, c555, "will soon be available!", 8, 112);
+    drawText(d, font, c555, "Bonus 100000pts per life!", 8, 160);
 }
