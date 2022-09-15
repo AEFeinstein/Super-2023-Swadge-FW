55fb24266dbb0d9a2a40a1785eadec3058b7414e
AEFeinstein <gelakinetic@gmail.com>
2022-09-15 01:06:14 -0400

    Add pyFlashGui.py & README.md Ignore .bin files Remove esptool submodule

---
+++ pyFlashGui/README.md
@@ -0,0 +1,50 @@
+# Compatibility
+
+This Python script should be compatible with Python 2 & 3. I have tested it on Windows with Python 3.
+
+# Summary
+
+This Python script polls for USB serial ports which match the VID & PID of an ESP32-S2. When one is found, it will attempt to flash it using `esptool`.
+
+The UI will be green when a Swadge is successfully flashed. It will be red if the flash failed.
+
+# Dependencies
+
+This script requires ``python3`` which can be installed from https://www.python.org/downloads/.
+
+The following Python modules must be installed:
+| Module  | URL | PIP |
+| ------------- | ------------- | ------------- |
+| ``tkinter``  | https://tkdocs.com/tutorial/install.html  | Tkinter (and, since Python 3.1, ttk, the interface to the newer themed widgets) is included in the Python standard library. | 
+| ``pyserial``  | https://pyserial.readthedocs.io/en/latest/pyserial.html  | ```pip install pyserial```| 
+| ``esptool`` | https://docs.espressif.com/projects/esptool/en/latest/esp32s2/installation.html | ```pip install esptool``` |
+
+For the script to actually flash firmware, the following files must be in a directory called ``bin`` at the same level as ``pyFlashGui.py``:
+
+| File  | Relative Path |
+| ------------- | ------------- |
+| ``bootloader.bin``  | ``../build/bootloader/bootloader.bin``  |
+| ``swadge-esp32.bin`` | ``../build/swadge-esp32.bin`` |
+| ``partition-table.bin`` | ``../build/partition_table/partition-table.bin`` |
+|``storage.bin`` | ``../build/storage.bin``|
+
+Assuming you've checked out the whole repository, set up the IDF, and are in the `pyFlashGui` folder, then this should build and place the files:
+
+```bash
+mkdir bin
+cd ../
+idf.py build
+cp ./build/bootloader/bootloader.bin ./build/swadge-esp32.bin ./build/partition_table/partition-table.bin ./build/storage.bin ./pyFlashGui/bin/
+```
+
+# Instructions
+
+1. Download and install the dependencies
+1. Download the final Swadge firmware from the Releases tab: https://github.com/AEFeinstein/Super-2023-Swadge-FW/releases/
+1. Run the programmer script (``python3 pyFlashGui.py`` from a terminal)
+1. Switch the Swadge's power to USB (right position)
+1. Hold the PGM button down (Up on the directional pad)
+1. Plug the Swadge into the computer with a USB-C cable
+1. Wait for the GUI to flash green, then you're done.
+
+TODO: Make instructional video (flash & test)
---
+++ pyFlashGui/pyFlashGui.py
@@ -0,0 +1,171 @@
+import serial.tools.list_ports
+import serial.tools.list_ports_common
+import threading
+import esptool
+try:
+    # for Python2
+    import Tkinter as tk
+except ImportError:
+    # for Python3
+    import tkinter as tk
+
+class LabelThread(threading.Thread):
+
+    def __init__(self, _serialPort: serial.tools.list_ports_common.ListPortInfo):
+        super(LabelThread, self).__init__()
+        self.serialPort = _serialPort
+        self.labelText = str(self.serialPort.device)
+        self.labelColor = "gray"
+
+    def getLabelText(self) -> str:
+        return self.labelText
+
+    def getLabelColor(self) -> str:
+        return self.labelColor
+
+    def run(self):
+        # Draw the label
+        self.labelText = "Attempting flash on " + str(self.serialPort.device)
+        self.labelColor = "gray"
+
+        # Try to flash the firmware
+        try:
+            esptool.main([
+                "--chip", "esp32s2",
+                "-p", str(self.serialPort.device),
+                "-b", "2000000",
+                "--before=default_reset",
+                "--after=no_reset_stub",
+                "write_flash",
+                "--flash_mode", "dio",
+                "--flash_freq", "80m",
+                "--flash_size", "4MB",
+                "0x1000",   "bins/bootloader.bin",
+                "0x10000",  "bins/swadge-esp32.bin",
+                "0x8000",   "bins/partition-table.bin",
+                "0x110000", "bins/storage.bin"])
+
+            # It worked! Display a nice green message
+            self.labelText = "Flash succeeded on " + str(self.serialPort.device)
+            self.labelColor = "green"
+        except Exception as e:
+            # It failed. Display a bad red message
+            self.labelText = "Flash failed on " + str(self.serialPort.device) + " because: " + str(e)
+            self.labelColor = "red"
+
+
+class ProgrammerApplication:
+
+    def init(self):
+        # Keep track of all the threads
+        self.threads: list[LabelThread] = []
+
+        # Keep a list of serial ports
+        self.swadgeSerialPorts: list[serial.tools.list_ports_common.ListPortInfo] = []
+
+        # Grid parameters
+        self.numRows: int = 0
+        self.numCols: int = 1
+
+        # Create the UI root
+        self.root = tk.Tk()
+
+        # Set up a menu, just to quit
+        menubar = tk.Menu(self.root)
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Exit", command=exit_function)
+        menubar.add_cascade(label="File", menu=filemenu)
+        self.root.config(menu=menubar)
+
+        # Start the main UI loop
+        self.root.winfo_toplevel().title("ESP32-S2 Swadge Programmer")
+
+        # Run this loop until it should quit
+        self.shouldQuit: bool = False
+        while not self.shouldQuit:
+
+            # First get a list of all current serial ports
+            serialPortList: list[serial.tools.list_ports_common.ListPortInfo] = serial.tools.list_ports.comports()
+            serialPort: serial.tools.list_ports_common.ListPortInfo
+
+            # Check for added serial ports
+            for serialPort in serialPortList:
+                # If the serial port exists but isn't in swadgeSerialPorts
+                if serialPort not in self.swadgeSerialPorts:
+                    # If the VID and PID match the ESP32-S2's values, use it
+                    if(serialPort.vid == 0x303A and serialPort.pid == 0x0002):
+                        self.swadgeSerialPorts.append(serialPort)
+                        # Then create a thread for esptool associated with this serial port and label and run it
+                        programmerThread = LabelThread(_serialPort=serialPort)
+                        self.threads.append(programmerThread)
+                        programmerThread.start()
+
+            # Check for removed serial ports
+            for serialPort in self.swadgeSerialPorts:
+                # If the serial port doesn't exist anymore
+                if serialPort not in serialPortList:
+                    # Remove it
+                    self.swadgeSerialPorts.remove(serialPort)
+                    # Stop any associated threads
+                    for thread in self.threads:
+                        if thread.serialPort == serialPort:
+                            thread.join()
+                            self.threads.remove(thread)
+
+            # Update UI
+            self.adjustRows()
+            self.root.update_idletasks()
+            self.root.update()
+
+    def exit(self):
+        if len(self.threads) > 0:
+            self.root.update()
+            # Wait for all threads to actually stop
+            for thread in self.threads:
+                thread.join()
+        # Destroy the UI
+        self.root.destroy()
+        self.shouldQuit = True
+
+    def adjustRows(self):
+        # Figure out how many rows and columns there are
+        self.numRows = len(self.threads)
+        self.numCols = 1
+
+        # Remove all entries from the grid
+        tk.Grid.grid_remove(self.root)
+        for widget in self.root.grid_slaves():
+            tk.Grid.grid_remove(widget)
+
+        # Set up rows and columns
+        for col in range(self.numCols):
+            tk.Grid.columnconfigure(self.root, col, weight=1)
+        for row in range(self.numRows):
+            tk.Grid.rowconfigure(self.root, row, weight=1)
+
+        # Add each label from the thread to the grid
+        rowIdx: int = 0
+        colIdx: int = 0
+        if len(self.threads) > 0:
+            # For each serial port
+            for thread in self.threads:
+                # Add label from the thread to the grid
+                serialPortLabel = tk.Label(self.root, text=thread.getLabelText(), bg=thread.getLabelColor())
+                serialPortLabel.grid(row=rowIdx, column=colIdx, sticky=[tk.W, tk.E, tk.N, tk.S])
+                colIdx = colIdx + 1
+                if(colIdx == self.numCols):
+                    colIdx = 0
+                    rowIdx = rowIdx + 1
+        else:
+            # No serial ports, print a reminder
+            serialPortLabel = tk.Label(self.root, text="Connect a Swadge")
+            serialPortLabel.grid(row=rowIdx, column=colIdx, sticky=[tk.W, tk.E, tk.N, tk.S])
+
+
+def exit_function():
+    app.exit()
+
+
+if __name__ == "__main__":
+    app = ProgrammerApplication()
+    app.init()
