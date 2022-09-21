a9f53870be1d24198c527cdf18ce68afa620b5bd
AEFeinstein <gelakinetic@gmail.com>
2022-09-20 20:23:50 -0400

    Emulate heap_caps_calloc

--- emu/src/emu_esp.c
+++ emu/src/emu_esp.c
@@ -178,3 +178,22 @@ void *heap_caps_malloc(size_t size, uint32_t caps __attribute__((unused)))
 {
     return malloc(size);
 }
+
+/**
+ * @brief Allocate a chunk of memory which has the given capabilities. The initialized value in the memory is set to zero.
+ *
+ * Equivalent semantics to libc calloc(), for capability-aware memory.
+ *
+ * In IDF, ``calloc(p)`` is equivalent to ``heap_caps_calloc(p, MALLOC_CAP_8BIT)``.
+ *
+ * @param n    Number of continuing chunks of memory to allocate
+ * @param size Size, in bytes, of a chunk of memory to allocate
+ * @param caps        Bitwise OR of MALLOC_CAP_* flags indicating the type
+ *                    of memory to be returned
+ *
+ * @return A pointer to the memory allocated on success, NULL on failure
+ */
+void *heap_caps_calloc(size_t n, size_t size, uint32_t caps)
+{
+    return calloc(n, size);
+}
