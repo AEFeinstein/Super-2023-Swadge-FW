cd9223b81b2068d07616bff5607e1d4ed4156a87
Dylan Whichard <dylan@whichard.com>
2022-09-22 00:38:25 -0400

    Parameterize and clean up color boxes

--- main/modes/mode_paint.c
+++ main/modes/mode_paint.c
@@ -742,6 +742,45 @@ void plotRectFilledTranslate(display_t* disp, int x0, int y0, int x1, int y1, pa
     fillDisplayArea(disp, xTr(x0), yTr(y0), xTr(x1 - 1), yTr(y1 - 1), col);
 }
 
+void drawColorBox(uint16_t xOffset, uint16_t yOffset, uint16_t w, uint16_t h, paletteColor_t col, bool selected, paletteColor_t topBorder, paletteColor_t bottomBorder)
+{
+    int dashLen = selected ? 1 : 0;
+    if (selected)
+    {
+        topBorder = c000;
+        bottomBorder = c000;
+    }
+
+    if (col == cTransparent)
+    {
+        // Draw a lil checkerboard
+        fillDisplayArea(paintState->disp, xOffset, yOffset, xOffset + w / 2, yOffset + h / 2, c111);
+        fillDisplayArea(paintState->disp, xOffset + w / 2, yOffset, xOffset + w, yOffset + h / 2, c555);
+        fillDisplayArea(paintState->disp, xOffset, yOffset + h / 2, xOffset + w / 2, yOffset + h, c555);
+        fillDisplayArea(paintState->disp, xOffset + w / 2, yOffset + h / 2, xOffset + w, yOffset + h, c111);
+    }
+    else
+    {
+        fillDisplayArea(paintState->disp, xOffset, yOffset, xOffset + w, yOffset + h, col);
+    }
+
+    if (topBorder != cTransparent)
+    {
+        // Top border
+        plotLine(paintState->disp, xOffset - 1,  yOffset, xOffset + w - 1, yOffset, topBorder, dashLen);
+        // Left border
+        plotLine(paintState->disp, xOffset - 1, yOffset, xOffset - 1, yOffset + h - 1, topBorder, dashLen);
+    }
+
+    if (bottomBorder != cTransparent)
+    {
+        // Bottom border
+        plotLine(paintState->disp, xOffset, yOffset + h, xOffset + w - 1, yOffset + h, bottomBorder, dashLen);
+        // Right border
+        plotLine(paintState->disp, xOffset + w, yOffset + 1, xOffset + w, yOffset + h - 1, bottomBorder, dashLen);
+    }
+}
+
 void paintRenderToolbar()
 {
     //////// Background
@@ -771,57 +810,24 @@ void paintRenderToolbar()
     plotRect(paintState->disp, PAINT_CANVAS_X_OFFSET - 1, PAINT_CANVAS_Y_OFFSET - 1, PAINT_CANVAS_X_OFFSET + paintState->canvasW * PAINT_CANVAS_SCALE + 1, PAINT_CANVAS_Y_OFFSET + paintState->canvasH * PAINT_CANVAS_SCALE + 1, c000);
 
 
-
-    int16_t activeColorOffsetX = 4;
-    int16_t activeColorOffsetY = 4;
-
-    // The number of vertical pixels between each color box (not including borders)
-    int16_t spaceBetween = 2;
-    // The width and height of the color box, not including borders
-    int16_t colorBoxSize = 8;
+    //////// Draw the active FG/BG colors and the color palette
 
     //////// Active Colors
-    fillDisplayArea(paintState->disp, activeColorOffsetX, PAINT_CANVAS_Y_OFFSET + activeColorOffsetY, activeColorOffsetX + colorBoxSize, PAINT_CANVAS_Y_OFFSET + activeColorOffsetY + colorBoxSize, paintState->bgColor);
-    fillDisplayArea(paintState->disp, activeColorOffsetX + colorBoxSize / 2, PAINT_CANVAS_Y_OFFSET + activeColorOffsetY + colorBoxSize / 2, activeColorOffsetX + colorBoxSize / 2 + colorBoxSize, PAINT_CANVAS_Y_OFFSET + activeColorOffsetY + colorBoxSize / 2 + colorBoxSize, paintState->fgColor);
-
-    // The starting X and Y of the first color box, not including borders
-    int16_t colorBoxStartY = PAINT_CANVAS_Y_OFFSET + 16 + spaceBetween * 2;
-    int16_t colorBoxStartX = PAINT_CANVAS_X_OFFSET / 2 - colorBoxSize / 2;
-
-    // The color to use for the top and bottom shadow / border
-    paletteColor_t topBorder, bottomBorder;
-    int dashLen;
+    // Draw the background color, then draw the foreground color overlapping it and offset by half in both directions
+    drawColorBox(PAINT_ACTIVE_COLOR_X, PAINT_ACTIVE_COLOR_Y, PAINT_COLORBOX_W, PAINT_COLORBOX_H, paintState->bgColor, false, PAINT_COLORBOX_SHADOW_TOP, PAINT_COLORBOX_SHADOW_BOTTOM);
+    drawColorBox(PAINT_ACTIVE_COLOR_X + PAINT_COLORBOX_W / 2, PAINT_ACTIVE_COLOR_Y + PAINT_COLORBOX_H / 2, PAINT_COLORBOX_W, PAINT_COLORBOX_H, paintState->fgColor, false, cTransparent, PAINT_COLORBOX_SHADOW_BOTTOM);
 
     //////// Recent Colors (palette)
     for (int i = 0; i < PAINT_MAX_COLORS; i++)
     {
-        // When SELECT is held and this is the selected color box, draw the border dashed and black
-        dashLen = (paintState->selectHeld && paintState->paletteSelect == i) ? 1 : 0;
-        topBorder = (paintState->selectHeld && paintState->paletteSelect == i) ? c000 : PAINT_COLORBOX_SHADOW_TOP;
-        bottomBorder = (paintState->selectHeld && paintState->paletteSelect == i) ? c000 : PAINT_COLORBOX_SHADOW_BOTTOM;
-
-        if (paintState->recentColors[i] == cTransparent)
+        if (PAINT_COLORBOX_HORIZONTAL)
         {
-            fillDisplayArea(paintState->disp, colorBoxStartX, colorBoxStartY + (spaceBetween + colorBoxSize) * i, colorBoxStartX + colorBoxSize / 2, colorBoxStartY + (spaceBetween + colorBoxSize) * i + colorBoxSize / 2, c111);
-            fillDisplayArea(paintState->disp, colorBoxStartX + (colorBoxSize / 2), colorBoxStartY + (spaceBetween + colorBoxSize) * i, colorBoxStartX + colorBoxSize, colorBoxStartY + (spaceBetween + colorBoxSize) * i + colorBoxSize / 2, c555);
-            fillDisplayArea(paintState->disp, colorBoxStartX, (colorBoxSize / 2) + colorBoxStartY + (spaceBetween + colorBoxSize) * i, colorBoxStartX + colorBoxSize / 2, colorBoxStartY + (spaceBetween + colorBoxSize) * i + colorBoxSize, c555);
-            fillDisplayArea(paintState->disp, colorBoxStartX + (colorBoxSize / 2), (colorBoxSize / 2) + colorBoxStartY + (spaceBetween + colorBoxSize) * i, colorBoxStartX + colorBoxSize, colorBoxStartY + (spaceBetween + colorBoxSize) * i + colorBoxSize, c111);
+            drawColorBox(PAINT_COLORBOX_X + i * (PAINT_COLORBOX_MARGIN_LEFT + PAINT_COLORBOX_W), PAINT_COLORBOX_Y, PAINT_COLORBOX_W, PAINT_COLORBOX_H, paintState->recentColors[i], paintState->selectHeld && paintState->paletteSelect == i, PAINT_COLORBOX_SHADOW_TOP, PAINT_COLORBOX_SHADOW_BOTTOM);
         }
         else
         {
-            fillDisplayArea(paintState->disp, colorBoxStartX, colorBoxStartY + (spaceBetween + colorBoxSize) * i, colorBoxStartX + colorBoxSize, colorBoxStartY + (spaceBetween + colorBoxSize) * i + colorBoxSize, paintState->recentColors[i]);
+            drawColorBox(PAINT_COLORBOX_X, PAINT_COLORBOX_Y + i * (PAINT_COLORBOX_MARGIN_TOP + PAINT_COLORBOX_H), PAINT_COLORBOX_W, PAINT_COLORBOX_H, paintState->recentColors[i], paintState->selectHeld && paintState->paletteSelect == i, PAINT_COLORBOX_SHADOW_TOP, PAINT_COLORBOX_SHADOW_BOTTOM);
         }
-
-
-        // Top border
-        plotLine(paintState->disp, colorBoxStartX - 1,  colorBoxStartY + i * (spaceBetween + colorBoxSize) - spaceBetween + 1, colorBoxStartX + colorBoxSize - 1, colorBoxStartY + i * (spaceBetween + colorBoxSize) - spaceBetween + 1, topBorder, dashLen);
-        // Left border
-        plotLine(paintState->disp, colorBoxStartX - 1, colorBoxStartY + i * (spaceBetween + colorBoxSize) - 1, colorBoxStartX - 1, colorBoxStartY + i * (spaceBetween + colorBoxSize) + colorBoxSize - 1, topBorder, dashLen);
-
-        // Bottom border
-        plotLine(paintState->disp, colorBoxStartX, colorBoxStartY + (spaceBetween + colorBoxSize) * i + colorBoxSize, colorBoxStartX + colorBoxSize - 1, colorBoxStartY + (spaceBetween + colorBoxSize) * i + colorBoxSize, bottomBorder, dashLen);
-        // Right border
-        plotLine(paintState->disp, colorBoxStartX + colorBoxSize, colorBoxStartY + (spaceBetween + colorBoxSize) * i, colorBoxStartX + colorBoxSize, colorBoxStartY + (colorBoxSize + spaceBetween) * i - spaceBetween + colorBoxSize + 1, bottomBorder, dashLen);
     }
 
     // Draw the brush name
--- main/modes/mode_paint.h
+++ main/modes/mode_paint.h
@@ -3,15 +3,47 @@
 
 #include "swadgeMode.h"
 
+// The number of colors in the palette and the max number of colors an image can be saved with
 #define PAINT_MAX_COLORS 16
+
+// The size of the buffer for loading/saving the image. Each chunk is saved as a separate blob in NVS
 #define PAINT_SAVE_CHUNK_SIZE 1024
+
+
+#define TFT_WIDTH 280
+#define TFT_HEIGHT 240
+
 #define PAINT_CANVAS_SCALE 3
-#define PAINT_CANVAS_WIDTH 70 // 70
-#define PAINT_CANVAS_HEIGHT 60 // 60
-#define PAINT_CANVAS_X_OFFSET 18
+#define PAINT_CANVAS_WIDTH 70
+#define PAINT_CANVAS_HEIGHT 60
+
+#define PAINT_CANVAS_X_OFFSET ((TFT_WIDTH - PAINT_CANVAS_WIDTH * PAINT_CANVAS_SCALE) / 2)
 #define PAINT_CANVAS_Y_OFFSET 26
+
+
+#define PAINT_TOOLBAR_BG c333
+
+// Set to 1 for horizontal colorboxes
+#define PAINT_COLORBOX_HORIZONTAL 0
+
+// Dimensions of the color boxes in the palette
+#define PAINT_COLORBOX_W 9
+#define PAINT_COLORBOX_H 9
+
 #define PAINT_COLORBOX_SHADOW_TOP c444
 #define PAINT_COLORBOX_SHADOW_BOTTOM c222
+
+#define PAINT_COLORBOX_MARGIN_TOP 2
+#define PAINT_COLORBOX_MARGIN_LEFT 2
+
+// X and Y position of the active color boxes (foreground/background color)
+#define PAINT_ACTIVE_COLOR_X ((PAINT_CANVAS_X_OFFSET - PAINT_COLORBOX_W - PAINT_COLORBOX_W / 2) / 2)
+#define PAINT_ACTIVE_COLOR_Y (PAINT_CANVAS_Y_OFFSET - PAINT_COLORBOX_H / 2)
+
+// X and Y position of the first palette color box
+#define PAINT_COLORBOX_X ((PAINT_CANVAS_X_OFFSET - PAINT_COLORBOX_W) / 2)
+#define PAINT_COLORBOX_Y (PAINT_ACTIVE_COLOR_Y + PAINT_COLORBOX_H * 2)
+
 
 // Convert from canvas coordinates to screen coordinates
 #define CNV2SCR_X(x) (PAINT_CANVAS_X_OFFSET + PAINT_CANVAS_SCALE * x)
@@ -21,10 +53,6 @@
 #define SCR2CNV_X(x) ((x - PAINT_CANVAS_X_OFFSET) / PAINT_CANVAS_SCALE)
 #define SCR2CNV_Y(y) ((y - PAINT_CANVAS_Y_OFFSET) / PAINT_CANVAS_SCALE)
 
-#define PAINT_TOOLBAR_BG c333
-
 extern swadgeMode modePaint;
-
-void setPaintMainMenu(bool resetPos);
 
 #endif
