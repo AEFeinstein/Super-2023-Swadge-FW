1f8015536b0dc052675060ebec5144b5c5dd1117
JVeg199X <swadgedev@github.com>
2022-09-27 09:11:48 -0400

    Revert "Fix broken readme tables"
    This reverts commit ae1f3b2868cc37a79143d4974d23d36a34f8025c.

--- main/swadge_esp32.c
+++ main/swadge_esp32.c
@@ -335,7 +335,7 @@ void mainSwadgeTask(void* arg __attribute((unused)))
             else
             {
                 // Otherwise enter test mode
-                cSwadgeMode = &modeMainMenu;
+                cSwadgeMode = &modeTest;
             }
             break;
         }
