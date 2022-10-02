410e6a76a532e67dc2cc6992ec83b1f250b5ac0f
AEFeinstein <gelakinetic@gmail.com>
2022-10-02 16:08:00 -0400

    Run emulator loop from taskYIELD() Rip out pthread from emulator Don't bundle libwinpthread-1.dll in artifact Remove all pthread mutex (display, sound, sensors)

--- emu/src/emu_esp.c
+++ emu/src/emu_esp.c
@@ -4,12 +4,12 @@
 
 #include <sys/time.h>
 #include <time.h>
-#include <pthread.h>
 #include <string.h>
 #include <unistd.h>
 
 #include "list.h"
 #include "emu_esp.h"
+#include "emu_main.h"
 
 #include "rmt.h"
 #include "touch_pad.h"
@@ -35,20 +35,6 @@
 //==============================================================================
 
 #define MAX_THREADS 10
-
-//==============================================================================
-// Variables
-//==============================================================================
-
-uint8_t pthreadIdx = 0;
-pthread_t threads[MAX_THREADS];
-volatile bool threadsShouldRun = true;
-
-//==============================================================================
-// Function prototypes
-//==============================================================================
-
-void * runTaskInThread(void * taskFnPtr);
 
 //==============================================================================
 // Functions
@@ -103,62 +89,9 @@ bool tud_ready(void)
  */
 void taskYIELD(void)
 {
+    emu_loop();
 	// Just sleep for ten ms
 	usleep(1000);
-}
-
-/**
- * @brief Helper function to call a TaskFunction_t from a pthread
- *
- * @param taskFnPtr The TaskFunction_t to call
- */
-void * runTaskInThread(void * taskFnPtr)
-{
-    // Cast and run!
-    ((TaskFunction_t)taskFnPtr)(NULL);
-    return NULL;
-}
-
-/**
- * @brief Run the given task immediately. The emulator only supports one task.
- *
- * @param pvTaskCode The task to immediately run
- * @param pcName unused
- * @param usStackDepth unused
- * @param pvParameters unused
- * @param uxPriority unused
- * @param pxCreatedTask unused
- * @return 0 for success, 1 for failure
- */
-BaseType_t xTaskCreate( TaskFunction_t pvTaskCode, const char * const pcName UNUSED,
-    const uint32_t usStackDepth UNUSED, void * const pvParameters UNUSED,
-    UBaseType_t uxPriority UNUSED, TaskHandle_t * const pxCreatedTask UNUSED)
-{
-    if(pthreadIdx < MAX_THREADS)
-    {
-        pthread_create(&threads[pthreadIdx++], NULL, runTaskInThread, pvTaskCode);
-    }
-    else
-    {
-        ESP_LOGE("TASK", "Emulator only supports %d tasks!", MAX_THREADS);
-    }
-
-    return 0;
-}
-
-/**
- * @brief Raise a flag to stop all threads, then join them
- */
-void joinThreads(void)
-{
-    // Tell threads to stop
-    threadsShouldRun = false;
-
-    // Wait for threads to actually stop
-    for(uint8_t i = 0; i < pthreadIdx; i++)
-    {
-        pthread_join(threads[i], NULL);
-    }
 }
 
 /**
