216e137d1029bd78fe935077fa563b0bdbe9ba44
Dylan Whichard <dylan@whichard.com>
2022-10-08 17:15:16 +0000

    Add eraseNvsKey in nvs_manager

--- components/hdw-nvs/nvs_manager.h
+++ components/hdw-nvs/nvs_manager.h
@@ -10,5 +10,6 @@ bool readNvs32(const char* key, int32_t* outVal);
 bool writeNvs32(const char* key, int32_t val);
 bool readNvsBlob(const char* key, void* out_value, size_t* length);
 bool writeNvsBlob(const char* key, const void* value, size_t length);
+bool eraseNvsKey(const char* key);
 
 #endif
