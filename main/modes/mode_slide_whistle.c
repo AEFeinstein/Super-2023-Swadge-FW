68b54bd6807c92a48920733e7ffc2914715d15e5
Brycey92 <bbrowner@optonline.net>
2022-10-23 03:37:12 -0400

    Finish implementing third octave in slide whistle

--- main/modes/mode_slide_whistle.c
+++ main/modes/mode_slide_whistle.c
@@ -44,13 +44,13 @@
 #define BAR_Y_MARGIN (slideWhistle->radiostars.h + CURSOR_HEIGHT + 2)
 #define BAR_X_WIDTH (slideWhistle->disp->w - (2 * BAR_X_MARGIN) - 1)
 
-/// Helper macro to return an integer clamped within a range (MIN to MAX)
-#define CLAMP(X, MIN, MAX) ( ((X) > (MAX)) ? (MAX) : ( ((X) < (MIN)) ? (MIN) : (X)) )
-#define lengthof(x) (sizeof(x) / sizeof(x[0]))
-
 #define REST_BIT 0x10000 // Largest note is 144, which is 0b10010000
 
 #define DEFAULT_PAUSE 5
+
+/// Helper macro to return an integer clamped within a range (MIN to MAX)
+#define CLAMP(X, MIN, MAX) ( ((X) > (MAX)) ? (MAX) : ( ((X) < (MIN)) ? (MIN) : (X)) )
+#define lengthof(x) (sizeof(x) / sizeof(x[0]))
 
 /*==============================================================================
  * Enums
@@ -224,43 +224,147 @@ const char mutedText[] =  "Swadge is muted!";
 const char playText[] =   ": Play";
 
 // All the scales
+#define NUM_OCTAVES 3
 #define LOWER_OCTAVE
+
 #ifdef LOWER_OCTAVE
-const noteFrequency_t scl_M_Penta[] = {C_4, D_4, E_4, G_4, A_4, C_5, C_5, D_5, E_5, G_5, A_5, C_6, };
-const noteFrequency_t scl_m_Penta[] = {C_4, D_SHARP_4, F_4, G_4, A_SHARP_4, C_5, C_5, D_SHARP_5, F_5, G_5, A_SHARP_5, C_6, };
-const noteFrequency_t scl_m_Blues[] = {C_4, D_SHARP_4, F_4, F_SHARP_4, G_4, A_SHARP_4, C_5, C_5, D_SHARP_5, F_5, F_SHARP_5, G_5, A_SHARP_5, C_6, };
-const noteFrequency_t scl_M_Blues[] = {C_4, D_4, D_SHARP_4, E_4, G_4, A_4, C_5, C_5, D_5, D_SHARP_5, E_5, G_5, A_5, C_6, };
-const noteFrequency_t scl_Major[] = {C_4, D_4, E_4, F_4, G_4, A_4, B_4, C_5, C_5, D_5, E_5, F_5, G_5, A_5, B_5, C_6, };
-const noteFrequency_t scl_Minor_Aeolian[] = {C_4, D_4, D_SHARP_4, F_4, G_4, G_SHARP_4, A_SHARP_4, C_5, C_5, D_5, D_SHARP_5, F_5, G_5, G_SHARP_5, A_SHARP_5, C_6, };
-const noteFrequency_t scl_Harm_Minor[] = {C_4, D_4, D_SHARP_4, F_4, G_4, G_SHARP_4, B_4, C_5, C_5, D_5, D_SHARP_5, F_5, G_5, G_SHARP_5, B_5, C_6, };
-const noteFrequency_t scl_Dorian[] = {C_4, D_4, D_SHARP_4, F_4, G_4, A_4, A_SHARP_4, C_5, C_5, D_5, D_SHARP_5, F_5, G_5, A_5, A_SHARP_5, C_6, };
-const noteFrequency_t scl_Phrygian[] = {C_4, C_SHARP_4, D_SHARP_4, F_4, G_4, G_SHARP_4, A_SHARP_4, C_5, C_5, C_SHARP_5, D_SHARP_5, F_5, G_5, G_SHARP_5, A_SHARP_5, C_6, };
-const noteFrequency_t scl_Lydian[] = {C_4, D_4, E_4, F_SHARP_4, G_4, A_4, B_4, C_5, C_5, D_5, E_5, F_SHARP_5, G_5, A_5, B_5, C_6, };
-const noteFrequency_t scl_Mixolydian[] = {C_4, D_4, E_4, F_4, G_4, A_4, A_SHARP_4, C_5, C_5, D_5, E_5, F_5, G_5, A_5, A_SHARP_5, C_6, };
-const noteFrequency_t scl_Locrian[] = {C_4, C_SHARP_4, D_SHARP_4, F_4, F_SHARP_4, G_SHARP_4, A_SHARP_4, C_5, C_5, C_SHARP_5, D_SHARP_5, F_5, F_SHARP_5, G_SHARP_5, A_SHARP_5, C_6, };
-const noteFrequency_t scl_Dom_Bebop[] = {C_4, D_4, E_4, F_4, G_4, A_4, A_SHARP_4, B_4, C_5, C_5, D_5, E_5, F_5, G_5, A_5, A_SHARP_5, B_5, C_6, };
-const noteFrequency_t scl_M_Bebop[] = {C_4, D_4, E_4, F_4, G_4, G_SHARP_4, A_SHARP_4, B_4, C_5, C_5, D_5, E_5, F_5, G_5, G_SHARP_5, A_SHARP_5, B_5, C_6, };
-const noteFrequency_t scl_Whole_Tone[] = {C_4, D_4, E_4, F_SHARP_4, G_SHARP_4, A_SHARP_4, C_5, C_5, D_5, E_5, F_SHARP_5, G_SHARP_5, A_SHARP_5, C_6, };
-const noteFrequency_t scl_Dacs[] = {C_4, D_SHARP_4, F_4, F_SHARP_4, G_4, A_4, C_5, D_SHARP_5, F_5, F_SHARP_5, G_5, A_5};
-const noteFrequency_t scl_Chromatic[] = {C_4, C_SHARP_4, D_4, D_SHARP_4, E_4, F_4, F_SHARP_4, G_4, G_SHARP_4, A_4, A_SHARP_4, B_4, C_5, C_5, C_SHARP_5, D_5, D_SHARP_5, E_5, F_5, F_SHARP_5, G_5, G_SHARP_5, A_5, A_SHARP_5, B_5, C_6, };
+const noteFrequency_t scl_M_Penta[] = {
+    C_4, D_4, E_4, G_4, A_4, C_5,
+    C_5, D_5, E_5, G_5, A_5, C_6,
+    C_6, D_6, E_6, G_6, A_6, C_7, };
+const noteFrequency_t scl_m_Penta[] = {
+    C_4, D_SHARP_4, F_4, G_4, A_SHARP_4, C_5,
+    C_5, D_SHARP_5, F_5, G_5, A_SHARP_5, C_6,
+    C_6, D_SHARP_6, F_6, G_6, A_SHARP_6, C_7, };
+const noteFrequency_t scl_m_Blues[] = {
+    C_4, D_SHARP_4, F_4, F_SHARP_4, G_4, A_SHARP_4, C_5,
+    C_5, D_SHARP_5, F_5, F_SHARP_5, G_5, A_SHARP_5, C_6,
+    C_6, D_SHARP_6, F_6, F_SHARP_6, G_6, A_SHARP_6, C_7, };
+const noteFrequency_t scl_M_Blues[] = {
+    C_4, D_4, D_SHARP_4, E_4, G_4, A_4, C_5,
+    C_5, D_5, D_SHARP_5, E_5, G_5, A_5, C_6,
+     C_6, D_6, D_SHARP_6, E_6, G_6, A_6, C_7, };
+const noteFrequency_t scl_Major[] = {
+    C_4, D_4, E_4, F_4, G_4, A_4, B_4, C_5,
+    C_5, D_5, E_5, F_5, G_5, A_5, B_5, C_6,
+    C_6, D_6, E_6, F_6, G_6, A_6, B_6, C_7, };
+const noteFrequency_t scl_Minor_Aeolian[] = {
+    C_4, D_4, D_SHARP_4, F_4, G_4, G_SHARP_4, A_SHARP_4, C_5,
+    C_5, D_5, D_SHARP_5, F_5, G_5, G_SHARP_5, A_SHARP_5, C_6,
+    C_6, D_6, D_SHARP_6, F_6, G_6, G_SHARP_6, A_SHARP_6, C_7, };
+const noteFrequency_t scl_Harm_Minor[] = {
+    C_4, D_4, D_SHARP_4, F_4, G_4, G_SHARP_4, B_4, C_5,
+    C_5, D_5, D_SHARP_5, F_5, G_5, G_SHARP_5, B_5, C_6,
+    C_6, D_6, D_SHARP_6, F_6, G_6, G_SHARP_6, B_6, C_7, };
+const noteFrequency_t scl_Dorian[] = {
+    C_4, D_4, D_SHARP_4, F_4, G_4, A_4, A_SHARP_4, C_5,
+    C_5, D_5, D_SHARP_5, F_5, G_5, A_5, A_SHARP_5, C_6,
+    C_6, D_6, D_SHARP_6, F_6, G_6, A_6, A_SHARP_6, C_7, };
+const noteFrequency_t scl_Phrygian[] = {
+    C_4, C_SHARP_4, D_SHARP_4, F_4, G_4, G_SHARP_4, A_SHARP_4, C_5,
+    C_5, C_SHARP_5, D_SHARP_5, F_5, G_5, G_SHARP_5, A_SHARP_5, C_6,
+    C_6, C_SHARP_6, D_SHARP_6, F_6, G_6, G_SHARP_6, A_SHARP_6, C_7, };
+const noteFrequency_t scl_Lydian[] = {
+    C_4, D_4, E_4, F_SHARP_4, G_4, A_4, B_4, C_5,
+    C_5, D_5, E_5, F_SHARP_5, G_5, A_5, B_5, C_6,
+    C_6, D_6, E_6, F_SHARP_6, G_6, A_6, B_6, C_7, };
+const noteFrequency_t scl_Mixolydian[] = {
+    C_4, D_4, E_4, F_4, G_4, A_4, A_SHARP_4, C_5,
+    C_5, D_5, E_5, F_5, G_5, A_5, A_SHARP_5, C_6,
+    C_6, D_6, E_6, F_6, G_6, A_6, A_SHARP_6, C_7, };
+const noteFrequency_t scl_Locrian[] = {
+    C_4, C_SHARP_4, D_SHARP_4, F_4, F_SHARP_4, G_SHARP_4, A_SHARP_4, C_5,
+    C_5, C_SHARP_5, D_SHARP_5, F_5, F_SHARP_5, G_SHARP_5, A_SHARP_5, C_6,
+    C_6, C_SHARP_6, D_SHARP_6, F_6, F_SHARP_6, G_SHARP_6, A_SHARP_6, C_7, };
+const noteFrequency_t scl_Dom_Bebop[] = {
+    C_4, D_4, E_4, F_4, G_4, A_4, A_SHARP_4, B_4, C_5,
+    C_5, D_5, E_5, F_5, G_5, A_5, A_SHARP_5, B_5, C_6,
+    C_6, D_6, E_6, F_6, G_6, A_6, A_SHARP_6, B_6, C_7, };
+const noteFrequency_t scl_M_Bebop[] = {
+    C_4, D_4, E_4, F_4, G_4, G_SHARP_4, A_SHARP_4, B_4, C_5,
+    C_5, D_5, E_5, F_5, G_5, G_SHARP_5, A_SHARP_5, B_5, C_6,
+    C_6, D_6, E_6, F_6, G_6, G_SHARP_6, A_SHARP_6, B_6, C_7, };
+const noteFrequency_t scl_Whole_Tone[] = {
+    C_4, D_4, E_4, F_SHARP_4, G_SHARP_4, A_SHARP_4, C_5,
+    C_5, D_5, E_5, F_SHARP_5, G_SHARP_5, A_SHARP_5, C_6,
+    C_6, D_6, E_6, F_SHARP_6, G_SHARP_6, A_SHARP_6, C_7, };
+const noteFrequency_t scl_Dacs[] = {
+    C_4, D_SHARP_4, F_4, F_SHARP_4, G_4, A_4,
+    C_5, D_SHARP_5, F_5, F_SHARP_5, G_5, A_5,
+    C_6, D_SHARP_6, F_6, F_SHARP_6, G_6, A_6, };
+const noteFrequency_t scl_Chromatic[] = {
+    C_4, C_SHARP_4, D_4, D_SHARP_4, E_4, F_4, F_SHARP_4, G_4, G_SHARP_4, A_4, A_SHARP_4, B_4, C_5,
+    C_5, C_SHARP_5, D_5, D_SHARP_5, E_5, F_5, F_SHARP_5, G_5, G_SHARP_5, A_5, A_SHARP_5, B_5, C_6,
+    C_6, C_SHARP_6, D_6, D_SHARP_6, E_6, F_6, F_SHARP_6, G_6, G_SHARP_6, A_6, A_SHARP_6, B_6, C_7, };
 #else
-const noteFrequency_t scl_M_Penta[] = {C_5, D_5, E_5, G_5, A_5, C_6, C_6, D_6, E_6, G_6, A_6, C_7, };
-const noteFrequency_t scl_m_Penta[] = {C_5, D_SHARP_5, F_5, G_5, A_SHARP_5, C_6, C_6, D_SHARP_6, F_6, G_6, A_SHARP_6, C_7, };
-const noteFrequency_t scl_m_Blues[] = {C_5, D_SHARP_5, F_5, F_SHARP_5, G_5, A_SHARP_5, C_6, C_6, D_SHARP_6, F_6, F_SHARP_6, G_6, A_SHARP_6, C_7, };
-const noteFrequency_t scl_M_Blues[] = {C_5, D_5, D_SHARP_5, E_5, G_5, A_5, C_6, C_6, D_6, D_SHARP_6, E_6, G_6, A_6, C_7, };
-const noteFrequency_t scl_Major[] = {C_5, D_5, E_5, F_5, G_5, A_5, B_5, C_6, C_6, D_6, E_6, F_6, G_6, A_6, B_6, C_7, };
-const noteFrequency_t scl_Minor_Aeolian[] = {C_5, D_5, D_SHARP_5, F_5, G_5, G_SHARP_5, A_SHARP_5, C_6, C_6, D_6, D_SHARP_6, F_6, G_6, G_SHARP_6, A_SHARP_6, C_7, };
-const noteFrequency_t scl_Harm_Minor[] = {C_5, D_5, D_SHARP_5, F_5, G_5, G_SHARP_5, B_5, C_6, C_6, D_6, D_SHARP_6, F_6, G_6, G_SHARP_6, B_6, C_7, };
-const noteFrequency_t scl_Dorian[] = {C_5, D_5, D_SHARP_5, F_5, G_5, A_5, A_SHARP_5, C_6, C_6, D_6, D_SHARP_6, F_6, G_6, A_6, A_SHARP_6, C_7, };
-const noteFrequency_t scl_Phrygian[] = {C_5, C_SHARP_5, D_SHARP_5, F_5, G_5, G_SHARP_5, A_SHARP_5, C_6, C_6, C_SHARP_6, D_SHARP_6, F_6, G_6, G_SHARP_6, A_SHARP_6, C_7, };
-const noteFrequency_t scl_Lydian[] = {C_5, D_5, E_5, F_SHARP_5, G_5, A_5, B_5, C_6, C_6, D_6, E_6, F_SHARP_6, G_6, A_6, B_6, C_7, };
-const noteFrequency_t scl_Mixolydian[] = {C_5, D_5, E_5, F_5, G_5, A_5, A_SHARP_5, C_6, C_6, D_6, E_6, F_6, G_6, A_6, A_SHARP_6, C_7, };
-const noteFrequency_t scl_Locrian[] = {C_5, C_SHARP_5, D_SHARP_5, F_5, F_SHARP_5, G_SHARP_5, A_SHARP_5, C_6, C_6, C_SHARP_6, D_SHARP_6, F_6, F_SHARP_6, G_SHARP_6, A_SHARP_6, C_7, };
-const noteFrequency_t scl_Dom_Bebop[] = {C_5, D_5, E_5, F_5, G_5, A_5, A_SHARP_5, B_5, C_6, C_6, D_6, E_6, F_6, G_6, A_6, A_SHARP_6, B_6, C_7, };
-const noteFrequency_t scl_M_Bebop[] = {C_5, D_5, E_5, F_5, G_5, G_SHARP_5, A_SHARP_5, B_5, C_6, C_6, D_6, E_6, F_6, G_6, G_SHARP_6, A_SHARP_6, B_6, C_7, };
-const noteFrequency_t scl_Whole_Tone[] = {C_5, D_5, E_5, F_SHARP_5, G_SHARP_5, A_SHARP_5, C_6, C_6, D_6, E_6, F_SHARP_6, G_SHARP_6, A_SHARP_6, C_7, };
-const noteFrequency_t scl_Dacs[] = {C_5, D_SHARP_5, F_5, F_SHARP_5, G_5, A_5, C_6, D_SHARP_6, F_6, F_SHARP_6, G_6, A_6};
-const noteFrequency_t scl_Chromatic[] = {C_5, C_SHARP_5, D_5, D_SHARP_5, E_5, F_5, F_SHARP_5, G_5, G_SHARP_5, A_5, A_SHARP_5, B_5, C_6, C_6, C_SHARP_6, D_6, D_SHARP_6, E_6, F_6, F_SHARP_6, G_6, G_SHARP_6, A_6, A_SHARP_6, B_6, C_7, };
+const noteFrequency_t scl_M_Penta[] = {
+    C_5, D_5, E_5, G_5, A_5, C_6,
+    C_6, D_6, E_6, G_6, A_6, C_7,
+    C_7, D_7, E_7, G_7, A_7, C_8, };
+const noteFrequency_t scl_m_Penta[] = {
+    C_5, D_SHARP_5, F_5, G_5, A_SHARP_5, C_6,
+    C_6, D_SHARP_6, F_6, G_6, A_SHARP_6, C_7,
+    C_7, D_SHARP_7, F_7, G_7, A_SHARP_7, C_8, };
+const noteFrequency_t scl_m_Blues[] = {
+    C_5, D_SHARP_5, F_5, F_SHARP_5, G_5, A_SHARP_5, C_6,
+    C_6, D_SHARP_6, F_6, F_SHARP_6, G_6, A_SHARP_6, C_7,
+    C_7, D_SHARP_7, F_7, F_SHARP_7, G_7, A_SHARP_7, C_8, };
+const noteFrequency_t scl_M_Blues[] = {
+    C_5, D_5, D_SHARP_5, E_5, G_5, A_5, C_6,
+    C_6, D_6, D_SHARP_6, E_6, G_6, A_6, C_7,
+     C_7, D_7, D_SHARP_7, E_7, G_7, A_7, C_8, };
+const noteFrequency_t scl_Major[] = {
+    C_5, D_5, E_5, F_5, G_5, A_5, B_5, C_6,
+    C_6, D_6, E_6, F_6, G_6, A_6, B_6, C_7,
+    C_7, D_7, E_7, F_7, G_7, A_7, B_7, C_8, };
+const noteFrequency_t scl_Minor_Aeolian[] = {
+    C_5, D_5, D_SHARP_5, F_5, G_5, G_SHARP_5, A_SHARP_5, C_6,
+    C_6, D_6, D_SHARP_6, F_6, G_6, G_SHARP_6, A_SHARP_6, C_7,
+    C_7, D_7, D_SHARP_7, F_7, G_7, G_SHARP_7, A_SHARP_7, C_8, };
+const noteFrequency_t scl_Harm_Minor[] = {
+    C_5, D_5, D_SHARP_5, F_5, G_5, G_SHARP_5, B_5, C_6,
+    C_6, D_6, D_SHARP_6, F_6, G_6, G_SHARP_6, B_6, C_7,
+    C_7, D_7, D_SHARP_7, F_7, G_7, G_SHARP_7, B_7, C_8, };
+const noteFrequency_t scl_Dorian[] = {
+    C_5, D_5, D_SHARP_5, F_5, G_5, A_5, A_SHARP_5, C_6,
+    C_6, D_6, D_SHARP_6, F_6, G_6, A_6, A_SHARP_6, C_7,
+    C_7, D_7, D_SHARP_7, F_7, G_7, A_7, A_SHARP_7, C_8, };
+const noteFrequency_t scl_Phrygian[] = {
+    C_5, C_SHARP_5, D_SHARP_5, F_5, G_5, G_SHARP_5, A_SHARP_5, C_6,
+    C_6, C_SHARP_6, D_SHARP_6, F_6, G_6, G_SHARP_6, A_SHARP_6, C_7,
+    C_7, C_SHARP_7, D_SHARP_7, F_7, G_7, G_SHARP_7, A_SHARP_7, C_8, };
+const noteFrequency_t scl_Lydian[] = {
+    C_5, D_5, E_5, F_SHARP_5, G_5, A_5, B_5, C_6,
+    C_6, D_6, E_6, F_SHARP_6, G_6, A_6, B_6, C_7,
+    C_7, D_7, E_7, F_SHARP_7, G_7, A_7, B_7, C_8, };
+const noteFrequency_t scl_Mixolydian[] = {
+    C_5, D_5, E_5, F_5, G_5, A_5, A_SHARP_5, C_6,
+    C_6, D_6, E_6, F_6, G_6, A_6, A_SHARP_6, C_7,
+    C_7, D_7, E_7, F_7, G_7, A_7, A_SHARP_7, C_8, };
+const noteFrequency_t scl_Locrian[] = {
+    C_5, C_SHARP_5, D_SHARP_5, F_5, F_SHARP_5, G_SHARP_5, A_SHARP_5, C_6,
+    C_6, C_SHARP_6, D_SHARP_6, F_6, F_SHARP_6, G_SHARP_6, A_SHARP_6, C_7,
+    C_7, C_SHARP_7, D_SHARP_7, F_7, F_SHARP_7, G_SHARP_7, A_SHARP_7, C_8, };
+const noteFrequency_t scl_Dom_Bebop[] = {
+    C_5, D_5, E_5, F_5, G_5, A_5, A_SHARP_5, B_5, C_6,
+    C_6, D_6, E_6, F_6, G_6, A_6, A_SHARP_6, B_6, C_7,
+    C_7, D_7, E_7, F_7, G_7, A_7, A_SHARP_7, B_7, C_8, };
+const noteFrequency_t scl_M_Bebop[] = {
+    C_5, D_5, E_5, F_5, G_5, G_SHARP_5, A_SHARP_5, B_5, C_6,
+    C_6, D_6, E_6, F_6, G_6, G_SHARP_6, A_SHARP_6, B_6, C_7,
+    C_7, D_7, E_7, F_7, G_7, G_SHARP_7, A_SHARP_7, B_7, C_8, };
+const noteFrequency_t scl_Whole_Tone[] = {
+    C_5, D_5, E_5, F_SHARP_5, G_SHARP_5, A_SHARP_5, C_6,
+    C_6, D_6, E_6, F_SHARP_6, G_SHARP_6, A_SHARP_6, C_7,
+    C_7, D_7, E_7, F_SHARP_7, G_SHARP_7, A_SHARP_7, C_8, };
+const noteFrequency_t scl_Dacs[] = {
+    C_5, D_SHARP_5, F_5, F_SHARP_5, G_5, A_5,
+    C_6, D_SHARP_6, F_6, F_SHARP_6, G_6, A_6,
+    C_7, D_SHARP_7, F_7, F_SHARP_7, G_7, A_7, };
+const noteFrequency_t scl_Chromatic[] = {
+    C_5, C_SHARP_5, D_5, D_SHARP_5, E_5, F_5, F_SHARP_5, G_5, G_SHARP_5, A_5, A_SHARP_5, B_5, C_6,
+    C_6, C_SHARP_6, D_6, D_SHARP_6, E_6, F_6, F_SHARP_6, G_6, G_SHARP_6, A_6, A_SHARP_6, B_6, C_7,
+    C_7, C_SHARP_7, D_7, D_SHARP_7, E_7, F_7, F_SHARP_7, G_7, G_SHARP_7, A_7, A_SHARP_7, B_7, C_8, };
 #endif
 
 const scale_t scales[] =
@@ -1332,10 +1436,10 @@ void  plotBar(uint8_t yOffset)
         c555, 0);
 
     // Plot tick marks at each of the note boundaries
-    for(uint8_t tick = 0; tick < (scales[slideWhistle->scaleIdx].notesLen / 2) + 1; tick++)
+    for(uint8_t tick = 0; tick < (scales[slideWhistle->scaleIdx].notesLen / NUM_OCTAVES) + 1; tick++)
     {
         int16_t x = BAR_X_MARGIN + ( (tick * ((slideWhistle->disp->w - 1) - (BAR_X_MARGIN * 2))) /
-                                     (scales[slideWhistle->scaleIdx].notesLen / 2)) ;
+                                     (scales[slideWhistle->scaleIdx].notesLen / NUM_OCTAVES)) ;
         plotLine(slideWhistle->disp, x, yOffset - TICK_HEIGHT,
                  x, yOffset + TICK_HEIGHT,
                  c555, 0);
@@ -1453,19 +1557,18 @@ noteFrequency_t  getCurrentNote(void)
     uint8_t noteIdx;
     if(slideWhistle->touchHeld)
     {
-        noteIdx = (slideWhistle->touchPosition * (scales[slideWhistle->scaleIdx].notesLen / 2)) / BAR_X_WIDTH;
+        noteIdx = (slideWhistle->touchPosition * (scales[slideWhistle->scaleIdx].notesLen / NUM_OCTAVES)) / BAR_X_WIDTH;
     }
     else
     {
-        noteIdx = (slideWhistle->roll * (scales[slideWhistle->scaleIdx].notesLen / 2)) / (BAR_X_WIDTH + 1);
+        noteIdx = (slideWhistle->roll * (scales[slideWhistle->scaleIdx].notesLen / NUM_OCTAVES)) / (BAR_X_WIDTH + 1);
     }
 
-    // See if we should play the higher note
+    // See which octave we should play
     if(slideWhistle->upHeld && !slideWhistle->downHeld)
     {
-        // TODO: Increase octave?? or re-jigger all of this to work off octaves instead of arbitrary notes
-        uint8_t offset = scales[slideWhistle->scaleIdx].notesLen / 2;
-        return scales[slideWhistle->scaleIdx].notes[noteIdx + offset];
+        uint8_t offset = scales[slideWhistle->scaleIdx].notesLen / NUM_OCTAVES;
+        return scales[slideWhistle->scaleIdx].notes[noteIdx + (offset * 2)];
     }
     else if(slideWhistle->downHeld && !slideWhistle->upHeld)
     {
@@ -1473,7 +1576,7 @@ noteFrequency_t  getCurrentNote(void)
     }
     else
     {
-        uint8_t offset = scales[slideWhistle->scaleIdx].notesLen / 2;
+        uint8_t offset = scales[slideWhistle->scaleIdx].notesLen / NUM_OCTAVES;
         return scales[slideWhistle->scaleIdx].notes[noteIdx + offset];
     }
 }
