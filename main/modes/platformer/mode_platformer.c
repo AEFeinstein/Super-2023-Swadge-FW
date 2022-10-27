17e22e9a8e550825626ed4f20c183721473a7c1e
JVeg199X <swadgedev@github.com>
2022-10-26 21:28:36 -0400

    Add 4-1,4-2 to level list

--- main/modes/platformer/mode_platformer.c
+++ main/modes/platformer/mode_platformer.c
@@ -951,7 +951,7 @@ swadgeMode modePlatformer =
         .overrideUsb = false
 };
 
-#define NUM_LEVELS 14
+#define NUM_LEVELS 15
 
 static leveldef_t leveldef[NUM_LEVELS] = {
     {.filename = "level1-1.bin",
@@ -990,7 +990,10 @@ static leveldef_t leveldef[NUM_LEVELS] = {
     {.filename = "level3-4.bin",
      .timeLimit = 220,
      .checkpointTimeLimit = 110},
-    {.filename = "level4-3.bin",
+    {.filename = "level4-1.bin",
+     .timeLimit = 240,
+     .checkpointTimeLimit = 90},
+    {.filename = "level4-2.bin",
      .timeLimit = 240,
      .checkpointTimeLimit = 90},
     {.filename = "debug.bin",
