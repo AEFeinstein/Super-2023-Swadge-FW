435e7f94542938ee3080ec23490af646aa136961
hdyar <bloop@hdyar.com>
2022-11-03 22:10:26 -0400

    Fixed music testing level to play when it's the real level

--- main/modes/picross/mode_picross.c
+++ main/modes/picross/mode_picross.c
@@ -407,7 +407,7 @@ void picrossGameLoop(int64_t elapsedUs)
     //You won! Only called once, since you cant go from win->solving without resetting everything (ie: menu and back)
     if(p->previousPhase == PICROSS_SOLVING && p->currentPhase == PICROSS_YOUAREWIN)
     {
-        if(p->selectedLevel.index == 0){
+        if(p->selectedLevel.index == 29){
             buzzer_play_bgm(&picross_music_rick);
         }
         //Unsave progress. Hides "current" in the main menu. we dont need to zero-out the actual data that will just happen when we load a new level.
