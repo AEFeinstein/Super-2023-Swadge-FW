4a5e7ebee68b7b27dbb73c14944ed68275621409
Brycey92 <bbrowner@optonline.net>
2022-11-04 23:32:49 -0400

    Move jumper perfect tune to jukebox music from sfx

--- main/modes/mode_jukebox.c
+++ main/modes/mode_jukebox.c
@@ -186,6 +186,7 @@ static const jukeboxSong jumperMusic[] =
     {.name = "Death", .song = &jumpDeathTune},
     {.name = "Game Over", .song = &jumpGameOverTune},
     {.name = "Winner", .song = &jumpWinTune},
+    {.name = "Perfect", .song = &jumpPerfectTune},
 };
 
 static const jukeboxSong creditsMusic[] =
@@ -268,7 +269,6 @@ static const jukeboxSong jumperSfx[] =
     {.name = "EvilDonut Jump", .song = &jumpEvilDonutJump},
     {.name = "Blump Jump", .song = &jumpBlumpJump},
     {.name = "Countdown", .song = &jumpCountdown},
-    {.name = "Perfect", .song = &jumpPerfectTune},
 };
 
 static const jukeboxSong tunernomesfx[] =
