d680ccb0ae145d64eda19c38eb256363114e305c
AEFeinstein <gelakinetic@gmail.com>
2022-11-08 22:37:46 -0500

    Move platformer sounds to C file Update jukebox songs

--- main/modes/platformer/mode_platformer.h
+++ main/modes/platformer/mode_platformer.h
@@ -15,30 +15,6 @@
 #define NUM_PLATFORMER_HIGH_SCORES 5
 
 //==============================================================================
-// Constants
-//==============================================================================
-
-extern const song_t sndHit;
-extern const song_t sndSquish;
-extern const song_t sndBreak;
-extern const song_t sndCoin;
-extern const song_t sndPowerUp;
-extern const song_t sndJump1;
-extern const song_t sndJump2;
-extern const song_t sndJump3;
-extern const song_t sndWarp;
-extern const song_t sndHurt;
-extern const song_t sndWaveBall;
-extern const song_t sndGameStart;
-extern const song_t sndDie;
-extern const song_t sndMenuSelect;
-extern const song_t sndMenuConfirm;
-extern const song_t sndMenuDeny;
-extern const song_t bgmDemagio;
-extern const song_t bgmIntro;
-extern const song_t bgmSmooth;
-
-//==============================================================================
 // Structs
 //==============================================================================
 
