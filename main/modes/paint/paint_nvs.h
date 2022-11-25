99bcf298f0a83c2ba819d1ae0b6c187ae2a9cc20
Dylan Whichard <dylan@whichard.com>
2022-11-25 16:11:25 -0500

    Refactor paint [de]serialization into functions

--- main/modes/paint/paint_nvs.h
+++ main/modes/paint/paint_nvs.h
@@ -20,6 +20,8 @@ void paintSetSlotInUse(int32_t* index, uint8_t slot);
 bool paintGetAnySlotInUse(int32_t index);
 uint8_t paintGetRecentSlot(int32_t index);
 void paintSetRecentSlot(int32_t* index, uint8_t slot);
+bool paintDeserialize(paintCanvas_t* dest, const uint8_t* data, size_t offset, size_t count);
+size_t paintSerialize(uint8_t* dest, const paintCanvas_t* canvas, size_t offset, size_t count);
 bool paintSave(int32_t* index, const paintCanvas_t* canvas, uint8_t slot);
 bool paintLoad(int32_t* index, paintCanvas_t* canvas, uint8_t slot);
 bool paintLoadDimensions(paintCanvas_t* canvas, uint8_t slot);
